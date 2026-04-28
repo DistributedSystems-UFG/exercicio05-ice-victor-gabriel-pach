ed89471952a04bbb84eee1b1edfbe0f405e4fecf
Victor <victorpachecogontijo@gmail.com>
2026-04-27 22:49:59 -0300

    alteracoes atividade

--- client.py
+++ client.py
@@ -1,11 +1,26 @@
 import sys, Ice
 import Demo
- 
-communicator = Ice.initialize(sys.argv) as communicator
 
-base = communicator.stringToProxy("SimplePrinter:default -p 11000")
-printer = Demo.PrinterPrx.checkedCast(base)
-if not printer:
-    raise RuntimeError("Invalid proxy")
+with Ice.initialize(sys.argv) as communicator:
 
-printer.printString("Hello World!")
+    # --- Printer object ---
+    base = communicator.stringToProxy("SimplePrinter:tcp -h <SERVER_PRIVATE_IP> -p 11000")
+    printer = Demo.PrinterPrx.checkedCast(base)
+    if not printer:
+        raise RuntimeError("Invalid proxy for Printer")
+
+    printer.printString("Hello from the client!")
+    echo = printer.echoString("Echo this string")
+    print("Echo reply:", echo)
+    n = printer.countChars("hello world")
+    print("countChars:", n)
+
+    # --- Calculator object (new server object) ---
+    base2 = communicator.stringToProxy("SimpleCalc:tcp -h <SERVER_PRIVATE_IP> -p 11000")
+    calc = Demo.CalculatorPrx.checkedCast(base2)
+    if not calc:
+        raise RuntimeError("Invalid proxy for Calculator")
+
+    print("10 + 3 =", calc.add(10, 3))
+    print("10 x 3 =", calc.multiply(10, 3))
+    print("10 - 3 =", calc.subtract(10, 3))
--- server.py
+++ server.py
@@ -1,15 +1,34 @@
 import sys, Ice
 import Demo
- 
+
 class PrinterI(Demo.Printer):
     def printString(self, s, current=None):
-        print(s)
+        print("printString:", s)
 
-communicator = Ice.initialize(sys.argv) 
+    def echoString(self, s, current=None):
+        print("echoString called with:", s)
+        return s
 
+    def countChars(self, s, current=None):
+        print("countChars called with:", s)
+        return len(s)
+
+class CalculatorI(Demo.Calculator):
+    def add(self, a, b, current=None):
+        return a + b
+
+    def multiply(self, a, b, current=None):
+        return a * b
+
+    def subtract(self, a, b, current=None):
+        return a - b
+
+communicator = Ice.initialize(sys.argv)
 adapter = communicator.createObjectAdapterWithEndpoints("SimpleAdapter", "default -p 11000")
-object = PrinterI()
-adapter.add(object, communicator.stringToIdentity("SimplePrinter"))
-adapter.activate()
 
+adapter.add(PrinterI(), communicator.stringToIdentity("SimplePrinter"))
+adapter.add(CalculatorI(), communicator.stringToIdentity("SimpleCalc"))
+
+adapter.activate()
+print("Server running on port 11000...")
 communicator.waitForShutdown()
